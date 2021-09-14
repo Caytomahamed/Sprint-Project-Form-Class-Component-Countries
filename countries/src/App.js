257772fcdad63a6d5874d5ebe19d40ff6fb47ff0
Aitoodeveloper <aidarous1143@gmail.com>
2021-09-14 23:08:59 +0300

    initial

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 // Import Components here
+import CountriesList from './components/CountriesList'
 
+import Form from './components/Form'
 import './App.css';
 
 
 class App extends Component {
 
   // Build Your constructor with state of "countries: []"
-
+  constructor(){
+    super()
+    this.state = {
+      countries: []
+    }
+  }
   // Use componentDidMount to make a call to API call to http://localhost:3333/countries and set it to the state.
 
 
+  componentDidMount(){
+    
+    axios.get('http://localhost:3333/countries').
+    then(res => {
+     const data = res.data
+     this.setState({
+       countries: data
+     })
+    })
+    
+  }
+
+
   render() {
+    // console.log(this.state.countries);
     return (
-      <div className="App">
-        <h1>COUNTRIES! </h1>
-        <div>Welcome to your Sprint Project</div>
-        <div>Start by reading the "README.md" and the comments in the components</div>
-        <div>Add your components here</div>
-        <div>Have fun!</div>
+      <div >
+        <div className='header'>
+        <div className='text'>
+        <div>
+        <h1>creating a Single Page </h1>
+        <span> Application That performs CRUD (Create, Read, Update, Delete) operations on a locally hosted API, Countries.</span>
+        </div>
+        <button  className='btn-header'>Add country now</button>
+        </div>       
+        </div>
+    
+        <CountriesList countries={this.state.countries} />
+        <Form />
       </div>
     );
   }
